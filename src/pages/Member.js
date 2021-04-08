083bed36004b9920447531fcf1c41f5715c2d557
Brittney Cavazos <brittney.cavazos@gmail.com>
2021-04-08 15:48:14 -0700

    stylized member page

--- src/pages/Member.js
+++ src/pages/Member.js
@@ -31,38 +31,39 @@ const MemberPage = () => {
 <div id='member' className='text-center'>
         <div className='row mt-4'> 
           <div className='col'>
-          <div className='card'>
+          <div className='card px-5 py-4 mt-5'>
           <div className='row no-gutters'> 
           <div className='col-md-5 th-img-containter'  >
           <div className='mem-card-bg-img' src={member.profile_img} style={{'backgroundImage': `url(${member.profile_img})`}}></div>
             </div>
           <div className='col-md-7 text-center th-card-body'>
-            <div className='th-card-header d-flex justify-content-center my-3'> 
+            <div className='th-card-header d-flex justify-content-center mt-5'> 
               <h1>{member.firstName} {member.lastName}</h1> 
             </div>
             
-            <div className='th-card-details'> 
-              <h3>{member.position}</h3>
+            <div className='th-card-details px-5'> 
+              <h4 className="text-secondary">{member.position}</h4>
             <div className='details my-3'>
-            <h4 className='text-secondary'>About</h4>
+            <h5 className='mt-4'>About</h5>
             <p className='hero-about'>{member.bio}</p>
             </div>
 
             <div className='detail'>
-                      <h4 className='text-secondary'>Languages:</h4>
+                      <h5 className='mt-4'>Languages:</h5>
                       <p className='my-2'>
                         {member.languages?.map((langs, i) => {
                           return (
-                            <span className='badge badge-pill badge-primary my-1' key={i}> <h5 className='my-1'> {langs} </h5></span>
+                            <span className='badge badge-pill badge-info mx-1 my-3 px-4' key={i}> <h5 className='my-1'> {langs} </h5></span>
                         )})}
                       </p>
                     </div>
 
             <div className='detail'>
-            <h4 className='text-secondary'>Contact:</h4>
-            <a href={member.linkedin} className='card-link' >Linkedin Profile</a>
-            <a href={member.email} className='card-link' >Email</a>
-            <a href={member.github} className='card-link' >Github Portfolio</a>
+            <h5 className='mt-4'>Contact:<br/><br/>
+            <a href={member.linkedin} className='card-link text-info' >Linkedin Profile</a>
+            <a href={member.email} className='card-link text-info' >Email</a>
+            <a href={member.github} className='card-link text-info' >Github Portfolio</a>
+            </h5>
 
 
             </div>
@@ -81,10 +82,15 @@ const MemberPage = () => {
     </div>
   </div>
 </div>
-<div className='row mt-5'>
+<div className="row mt-5 text-secondary small">
+  <div className="col-2">
+    See the rest of the team:
+  </div>
+</div>
+<div className='row mb-5'>
       {teamData.map((member, index) => {
         return (
-          <div className='col-1'>
+          <div className='col-1 mr-2'>
 
           <JumpLinks member={member} />
 
